d65875b8e073eec024f65d1e4f585d8ab4cc0452
atcecil01 <atcecil@gmail.com>
2025-09-25 15:44:30 -0400

    Add more styling to header

--- buttercupbakery.client/src/Components/HeaderComponent.jsx
+++ buttercupbakery.client/src/Components/HeaderComponent.jsx
@@ -4,7 +4,14 @@ import { NavLink } from 'react-router-dom'
 function Header() {
     return(
         <Fragment>
-            <h1 id='maintitle'>Buttercup Bakery</h1>
+            <h1 id='maintitle' className="scallop-bottom">
+                <div className="row">
+                    <div className="col-md-2" style={{ height: '.5em', margin: '3px 10px 0px 0px', backgroundColor: 'var(--header-color-1)' }} />
+                    <div className="col-md-2" style={{ height: '.5em', margin: '3px 10px 0px 0px', backgroundColor: 'var(--header-color-2)' }} />
+                    <div className="col-md-2" style={{ height: '.5em', margin: '3px 10px 0px 0px', backgroundColor: 'var(--header-color-3)' }} />
+                    <div className="col-md-5" style={{ height: '.5em', margin: '3px 10px 0px 0px', backgroundColor: 'var(--header-color-4)' }} />
+                </div>
+                Buttercup Bakery</h1>
             <nav className='navbar'>
                 <NavLink to='/Home' className={({ isActive, isPending }) => isActive ? 'navlink-active px-5' : 'navlink px-5'}><div className="square" />Home</NavLink>
                 <NavLink to='/About' className={({ isActive, isPending }) => isActive ? 'navlink-active px-5' : 'navlink px-5'}><div className="square" />About</NavLink>
